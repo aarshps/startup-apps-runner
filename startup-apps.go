57bfd9dbaf4026b614c29ac15092d9d62e70a086
Aarsh <aarshps@gmail.com>
2021-07-12 19:26:06 +0530

    Removed SmartGit start

--- startup-apps.go
+++ startup-apps.go
@@ -10,7 +10,6 @@ func main() {
 	fmt.Println("Starting Apps...")
 
 	startApp("Teams", "C:\\Users\\aps\\AppData\\Local\\Microsoft\\Teams\\Update.exe", ([]string{"--processStart", "Teams.exe"})...)
-	startApp("SmartGit", "C:\\Program Files\\SmartGit\\bin\\smartgit.exe")
 	startApp("Outlook", "C:\\Program Files (x86)\\Microsoft Office\\root\\Office16\\OUTLOOK.EXE")
 	startApp("Docker Desktop", "C:\\Program Files\\Docker\\Docker\\Docker Desktop.exe")
 	startApp("ConEmu", "C:\\Program Files\\ConEmu\\ConEmu64.exe")
